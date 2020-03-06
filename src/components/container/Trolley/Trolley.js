4e20941092fea3de9787937f6c9b612dc6c49d5c
Eky Umroh.com <ekyzulianto@umroh.com>
2020-03-06 14:41:31 +0700

    create trolley page

--- src/components/container/Trolley/Trolley.js
+++ src/components/container/Trolley/Trolley.js
@@ -3,8 +3,17 @@ import React from 'react';
 const Trolley = () => {
 
     return (
-        <div className="trolley">
+        <div className="keranjang">
             <h2>Keranjang Belanja</h2>
+            <ul>
+                <li>Nama Barang : Daging Ayam Berbumbu Rasa original Plus Tepung Crispy</li>
+                <li>Harga : Rp. 140.000,- [1 Karton - 10 Pack]</li>
+                <li>Jumlah Pesan : 5 Karton</li>
+            </ul>
+            <hr/>
+            <div className="total">
+                Total : Rp.500.000
+            </div>
         </div>
     )
 }
