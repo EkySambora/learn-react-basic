764062758cd6d0fbf4e08b6b9b20fddda98db2b0
Eky Umroh.com <ekyzulianto@umroh.com>
2020-04-04 16:14:44 +0700

    update

--- src/components/container/IndexPage/IndexPage.js
+++ src/components/container/IndexPage/IndexPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Home from '../Home/Home';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Trolley from '../Trolley/Trolley';
 
 const styled = {
